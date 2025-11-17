d57f7f303294b49d5b5ce29fd54a4ac6f13f9aa9
Daria-Dominiko <ddaasshhaa.com@gmail.com>
2025-11-18 01:40:28 +0300

    Урок 3 дополнение

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,13 +1,9 @@
 from address import Address
 from mailing import Mailing
 
-mailing = Mailing("{address1}",
-                  "{address2}",
+mailing = Mailing({177056, "Москва", "Пушкина", 8, 56},
+                  {177157, "Москва", "Маршала Жукова", 12, 79},
                   5237,
                   "H7563O9867G")
 
-address1 = Address(177056, "Москва", "Пушкина", 8, 56)
-
-address2 = Address(177157, "Москва", "Маршала Жукова", 12, 79)
-
-print(f'Отправление {track} из {from_address} - в {to_address}. Стоимость {cost} рублей.')
+print(f"Отправление {track} из {Address} - в {Address}. Стоимость {cost} рублей.")
