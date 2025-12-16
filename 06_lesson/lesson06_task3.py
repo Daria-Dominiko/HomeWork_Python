763bb0f18b245aaa0e41144f59d0dc5f6dd5ff58
Daria-Dominiko <ddaasshhaa.com@gmail.com>
2025-12-17 02:36:10 +0300

    урок 6 доп

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -1,23 +1,20 @@
 import waiter
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 driver = webdriver.Chrome()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-element = WebDriverWait(driver, 30).until(
-        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".lead" ), "Done!")
-        )
+waiter = WebDriverWait(driver, 30)
+waiter.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "p.lead"), "Done!"))
 
-container = driver.find_element(By.CSS_SELECTOR, ".col-12.py-2")
+element = driver.find_element(
+    By.CSS_SELECTOR, "#award").get_attribute("src")
 
-alt = driver.find_element(By.CSS_SELECTOR, "#award")
-
-print(alt.get_attribute("src"))
+print(element)
 
 driver.quit()
