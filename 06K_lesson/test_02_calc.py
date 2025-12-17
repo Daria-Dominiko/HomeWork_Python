e40fb12e7bb330b9a722195add2ec679909de0e7
Daria-Dominiko <ddaasshhaa.com@gmail.com>
2025-12-17 19:37:20 +0300

    контрольная

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,29 +1,29 @@
-# from time import sleep
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import waiter
-#
-# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-# driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-#
-# delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
-# delay_input.clear()
-# delay_input.send_keys("45")
-#
-# driver.find_element(By.XPATH, "//span[text()='7']").click()
-# driver.find_element(By.XPATH, "//span[text()='+']").click()
-# driver.find_element(By.XPATH, "//span[text()='8']").click()
-# driver.find_element(By.XPATH, "//span[text()='=']").click()
-#
-# waiter = WebDriverWait(driver, 46)
-# waiter.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "[role='status']")))
-#
-# result_element = driver.find_element(By.CSS_SELECTOR, "[class='screen']")
-# result_text = result_element.text
-# assert result_text == "15"
-#
-# driver.quit()
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import waiter
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
+delay_input.clear()
+delay_input.send_keys("45")
+
+driver.find_element(By.XPATH, "//span[text()='7']").click()
+driver.find_element(By.XPATH, "//span[text()='+']").click()
+driver.find_element(By.XPATH, "//span[text()='8']").click()
+driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+waiter = WebDriverWait(driver, 46)
+waiter.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "[role='status']")))
+
+result_element = driver.find_element(By.CSS_SELECTOR, "[class='screen']")
+result_text = result_element.text
+assert result_text == "15"
+
+driver.quit()
